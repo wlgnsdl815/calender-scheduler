2d5aaa13391c38b9f686c4636e7fb73aa30dbc54
wlgnsdl815 <wlgnsdl815@gmail.com>
2023-01-04 20:18:21 +0900

    Settings + theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     ),

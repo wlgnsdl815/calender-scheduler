8102c736becf12987c2e4c423461640819c1405a
wlgnsdl815 <wlgnsdl815@gmail.com>
2023-01-07 16:30:38 +0900

    Used Wrap Widget

--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -1,3 +1,5 @@
+import 'package:calender_scheduler/component/custom_text_field.dart';
+import 'package:calender_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 
 class ScheduleBottomSheet extends StatelessWidget {
@@ -15,12 +17,115 @@ class ScheduleBottomSheet extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.only(bottom: bottomInset),
         // 패딩추가
-        child: Column(
-          children: [
-            TextField(),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 8.0,
+            right: 8.0,
+            top: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _Time(),
+              // TextField가 올라오지 않을때는 CMD + Shift + K 누르면 올라온다
+              SizedBox(height: 16.0),
+              _Content(),
+              SizedBox(height: 16.0),
+              _ColorPicker(),
+              SizedBox(height: 8.0),
+              _SaveButton(),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _Time extends StatelessWidget {
+  const _Time({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: CustomTextField(
+            label: '시작 시간',
+          ),
+        ),
+        SizedBox(width: 16.0),
+        Expanded(
+          child: CustomTextField(
+            label: '마감 시간',
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Content extends StatelessWidget {
+  const _Content({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextField(
+      label: '내용',
+    );
+  }
+}
+
+class _ColorPicker extends StatelessWidget {
+  const _ColorPicker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      // Row 대신 Wrap을 사용하면 화면을 넘어갈 때 자동 줄 바꿈이 된다
+      spacing: 8.0, // 좌우 간격
+      runSpacing: 10.0, // 위 아래로위 간격
+      children: [
+        renderColor(Colors.red),
+        renderColor(Colors.orange),
+        renderColor(Colors.yellow),
+        renderColor(Colors.green),
+        renderColor(Colors.blue),
+        renderColor(Colors.indigo),
+        renderColor(Colors.purple),
+      ],
+    );
+  }
+
+  Widget renderColor(Color color) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color,
+      ),
+      width: 32.0,
+      height: 32.0,
+    );
+  }
+}
+
+class _SaveButton extends StatelessWidget {
+  const _SaveButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: PRIMARY_COLOR,
+            ),
+            child: Text('저장'),
+          ),
+        ),
+      ],
     );
   }
 }

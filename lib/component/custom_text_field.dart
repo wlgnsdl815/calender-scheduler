22d4b52f7486475ef68bc03d12e71f1868ad281f
wlgnsdl815 <wlgnsdl815@gmail.com>
2023-01-07 16:48:58 +0900

    TextField done

--- lib/component/custom_text_field.dart
+++ lib/component/custom_text_field.dart
@@ -1,10 +1,18 @@
 import 'package:calender_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomTextField extends StatelessWidget {
   final String label;
 
-  const CustomTextField({Key? key, required this.label}) : super(key: key);
+  // true - 시간 // false - 내용
+  final bool isTime;
+
+  const CustomTextField({
+    Key? key,
+    required this.label,
+    required this.isTime,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,15 +26,38 @@ class CustomTextField extends StatelessWidget {
             fontWeight: FontWeight.w600,
           ),
         ),
-        TextField(
-          cursorColor: Colors.grey,
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            filled: true,
-            fillColor: Colors.grey[300],
+        if (isTime) rendDerTextField(),
+        if (!isTime)
+          Expanded(
+            child: rendDerTextField(),
           ),
-        ),
       ],
+    );
+  }
+
+  Widget rendDerTextField() {
+    return TextField(
+      expands: !isTime,
+      cursorColor: Colors.grey,
+
+      maxLines: isTime ? 1 : null,
+      // 최대 줄의 갯수 null이면 줄이 무한히 내려간다
+      keyboardType: isTime ? TextInputType.number : TextInputType.multiline,
+      // 시간이면 숫자입력, 시간이 아니면 여러줄로 글을 쓸 수 있는 기능인 multiline
+
+      inputFormatters: isTime
+          ? [
+              // 블루투스 키보드 같은 걸 사용했을 때 문자 입력이 되는 걸 방지
+              FilteringTextInputFormatter.digitsOnly
+            ]
+          : [
+              // 아무것도 작성하지 않으면 제약이 없다
+            ],
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        filled: true,
+        fillColor: Colors.grey[300],
+      ),
     );
   }
 }
--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -10,31 +10,39 @@ class ScheduleBottomSheet extends StatelessWidget {
     final bottomInset = MediaQuery.of(context).viewInsets.bottom;
     // viewInsets.bottom을 화면 시스템때문에 가려진 부분의 픽셀을 구할 수 있다.
 
-    return Container(
-      color: Colors.white,
-      height: MediaQuery.of(context).size.height / 2 + bottomInset,
-      // 키보드 가린 만큼 더해주고
-      child: Padding(
-        padding: EdgeInsets.only(bottom: bottomInset),
-        // 패딩추가
-        child: Padding(
-          padding: const EdgeInsets.only(
-            left: 8.0,
-            right: 8.0,
-            top: 16.0,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _Time(),
-              // TextField가 올라오지 않을때는 CMD + Shift + K 누르면 올라온다
-              SizedBox(height: 16.0),
-              _Content(),
-              SizedBox(height: 16.0),
-              _ColorPicker(),
-              SizedBox(height: 8.0),
-              _SaveButton(),
-            ],
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+        // 화면 빈 곳을 눌렀을 때 키보드 닫힘 하기 위해서는 GestureDetector로 만들어야 함
+      },
+      child: SafeArea(
+        child: Container(
+          color: Colors.white,
+          height: MediaQuery.of(context).size.height / 2 + bottomInset,
+          // 키보드 가린 만큼 더해주고
+          child: Padding(
+            padding: EdgeInsets.only(bottom: bottomInset),
+            // 패딩추가
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 8.0,
+                right: 8.0,
+                top: 16.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _Time(),
+                  // TextField가 올라오지 않을때는 CMD + Shift + K 누르면 올라온다
+                  SizedBox(height: 16.0),
+                  _Content(),
+                  SizedBox(height: 16.0),
+                  _ColorPicker(),
+                  SizedBox(height: 8.0),
+                  _SaveButton(),
+                ],
+              ),
+            ),
           ),
         ),
       ),
@@ -51,12 +59,14 @@ class _Time extends StatelessWidget {
       children: [
         Expanded(
           child: CustomTextField(
+            isTime: true,
             label: '시작 시간',
           ),
         ),
         SizedBox(width: 16.0),
         Expanded(
           child: CustomTextField(
+            isTime: true,
             label: '마감 시간',
           ),
         ),
@@ -70,8 +80,11 @@ class _Content extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomTextField(
-      label: '내용',
+    return Expanded(
+      child: CustomTextField(
+        isTime: false,
+        label: '내용',
+      ),
     );
   }
 }

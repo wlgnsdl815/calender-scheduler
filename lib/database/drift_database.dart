47c19318819347293c34c73becf4d6c7732a31da
wlgnsdl815 <wlgnsdl815@gmail.com>
2023-01-10 15:34:56 +0900

    Add query

--- lib/database/drift_database.dart
+++ lib/database/drift_database.dart
@@ -25,6 +25,24 @@ part 'drift_database.g.dart';
 // 나중에 flutter에서 이 class 이름을 보고 앞에 _$를 붙여서 새로운 클래스를 part한 파일 안에 만든다
 class LocalDatabase extends _$LocalDatabase {
   LocalDatabase() : super(_openConnection());
+
+  // 쿼리 작성 - insert schedules
+  Future<int> createSchedule(SchedulesCompanion data) =>
+      into(schedules).insert(data);
+
+  // 쿼리 작성 - insert colors
+  Future<int> createCategoryColor(CategoryColorsCompanion data) =>
+      into(categoryColors).insert(data);
+
+  // 쿼리 작성 - get colors
+  Future<List<CategoryColor>> getCategoryColors() =>
+      select(categoryColors).get();
+
+  // 데이터 베이스의 테이블이 바뀌면 schemaVersion도 올려주어야 한다. 초기에는 1로 둔다
+  // .g.dart 파일이 생성 되고나면 아래 코드를 작성 해 주어야한다. class 이름에 자동완성 해도 뜨긴 한다
+  @override
+  // TODO: implement schemaVersion
+  int get schemaVersion => 1;
 }
 
 // 하드 드라이브의 어떤 위치에 저장할 지 명시해주어야 한다

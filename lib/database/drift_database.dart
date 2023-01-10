ec5f99036ce1efcb57249e06e0791bfcc1cde553
wlgnsdl815 <wlgnsdl815@gmail.com>
2023-01-10 15:10:21 +0900

    Created drift_database.dart file

---
+++ lib/database/drift_database.dart
@@ -0,0 +1,42 @@
+// private 값들은 불러올 수 없다
+import 'dart:io';
+
+import 'package:calender_scheduler/model/category_color.dart';
+import 'package:calender_scheduler/model/schedule.dart';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+// private 값까지 불러올 수 있다
+// Database를 연결할 때 part 선언을 해주어야 한다
+// 현재파일의 이름 .g.dart
+part 'drift_database.g.dart';
+
+//
+@DriftDatabase(
+  tables: [
+    // 괄호 없이 작성한다
+    Schedules,
+    CategoryColors,
+  ],
+)
+// 사용할 데이터베이스의 이름을 짓고 그 데이터베이스의 이름을 그대로 본따서 extends를 하고 앞에 _$를 붙여준다
+// 나중에 flutter에서 이 class 이름을 보고 앞에 _$를 붙여서 새로운 클래스를 part한 파일 안에 만든다
+class LocalDatabase extends _$LocalDatabase {
+  LocalDatabase() : super(_openConnection());
+}
+
+// 하드 드라이브의 어떤 위치에 저장할 지 명시해주어야 한다
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    // dbFolder라는 스트림 값 안에 앱을 특정 기기에 설치했을 때 앱 전용으로 사용 할 수 있는 폴더를 가져 올 수 있다
+    final dbFolder = await getApplicationDocumentsDirectory();
+
+    // 위 코드로 배정받은 폴더에다가 dbFolder.path로 경로를 가져왔고 'da.splite'라는 파일을 생성한 것이다
+    final file = File(p.join(dbFolder.path, 'db.splite'));
+
+    // 파일로 데이터베이스를 만들면 데이터베이스 생성 끝
+    return NativeDatabase(file);
+  });
+}
